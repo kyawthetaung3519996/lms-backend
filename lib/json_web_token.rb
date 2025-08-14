6c3df9cf1e0b8274322536735be8fcd04af9c4a7
kyawthetaung <kyawthetaung3519@gmail.com>
2025-08-14 11:40:06 +0700

    update for jwt secret key

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,5 +1,5 @@
 class JsonWebToken
-  JWT_SECRET = Rails.application.credentials.dig(:secret_key_base)
+  JWT_SECRET = ENV['SECRET_KEY_BASE'] || Rails.application.credentials.secret_key_base
  
   def self.encode(payload, exp = 12.hours.from_now)
     payload[:exp] = exp.to_i
